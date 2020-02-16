b5d5ea6e55d6c7c6c63bcd44fadaa2ff543bc32e
Christian Spencer <cjtspencer@gmail.com>
2020-02-16 16:32:41 -0600

    Updated .gitignore and added functionality to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include "main.h"
 using namespace std;
 
 int main(){
 
+    int input;
+    bool flag = true;
 
+    enum Types
+        { Fire = 1, Water, Earth};
 
+    cout << "Enter a number 1 - 3: ";
+    cin >> input;
+    while (flag){
+            
+            if (input > 3 || input < 1 )
+                cout << "\n\nThe number you have entered is not within the given range, try again.\nEnter a number 1-3: ";
+            else
+            {
+                flag = false;
+            }
+            
+
+    }
+    switch (input)
+    {
+        case Fire: cout << "Your type is Fire";
+        break;
+
+        case Water: cout << "Your type is Water";
+        break;
+
+        case Earth: cout << "Your type is Earth";
+    }
 }
 ;
